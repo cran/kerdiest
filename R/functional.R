f2e4755b907eb754ba83aba37d43e24c3b1f32d8
Ignacio López-de-Ullibarri <ignacio.lopezdeullibarri@udc.es>
2025-06-23 10:20:06 +0000

    version 1.3-1

--- R/functional.R
+++ R/functional.R
@@ -1,7 +1,5 @@
-functional <-
-function(type_kernel, y1,x1,bandwidth,pvalue)
-   {
-  a<-kde(type_kernel=type_kernel, vec_data=x1, y=y1, bw=bandwidth)$Estimated_values
-  b<-pvalue
-  return(a-b)
-  }
+functional <- function(type_kernel, y1, x1, bandwidth, pvalue) {
+  a <- kde(type_kernel = type_kernel, vec_data = x1, y = y1, bw = bandwidth)$Estimated_values
+  b <- pvalue
+  a - b
+}
--- R/kernel_function.R
+++ R/kernel_function.R
@@ -1,51 +1,36 @@
-kernel_function <-
-function(type_kernel,u)
-# INPUTS:
-#   "type_kernel" kernel function: "e" Epanechnikov,	"n" Normal, 
-#                                  "b" Biweight, "t" Triweight            
-#   "u" array or single value where the kernel is evaluated
-
-{
-	if(type_kernel == "e")	
-	{
-	  result <- u
-        logic0 <- (u <= -1 | u >= 1)
-    		logic1 <- (u > -1 & u < 1)
-		    result[logic0] <- 0
-	  Uval <- result[logic1]
+kernel_function <- function(type_kernel, u) {
+  if (type_kernel == "e") {
+    result <- u
+    logic0 <- (u <= -1 | u >= 1)
+    logic1 <- (u > -1 & u < 1)
+    result[logic0] <- 0
+    Uval <- result[logic1]
     result[logic1] <- 0.75 * (1 - (Uval^2))
-    return(result)
-	}
-
-	else 
-		if(type_kernel == "n")	
-		{
-		result <- dnorm(u)
-		return(result)
-		}
-
-	else 
-		if(type_kernel == "b")	
-		{
-		  result <- u
-         	logic0 <- (u <= -1 | u >= 1)
-    		  logic1 <- (u > -1 & u < 1)
-		      result[logic0] <- 0
-		  Uval <- result[logic1]
-      result[logic1] <- (15/16) * ((1 - (Uval^2)))^2
-      return(result)
-	  }
-		 
-	else 
-		if(type_kernel == "t")	
-		{
-		  result <- u
-         	logic0 <- (u <= -1 | u >= 1)
-    		  logic1 <- (u > -1 & u < 1)
-		      result[logic0] <- 0
-		  Uval <- result[logic1]
-      result[logic1] <- (35/32) * ((1 - (Uval^2)))^3
-      return(result)
-	  }
-
+    result
+  } else {
+    if (type_kernel == "n") {
+      result <- dnorm(u)
+      result
+    } else {
+      if (type_kernel == "b") {
+        result <- u
+        logic0 <- (u <= -1 | u >= 1)
+        logic1 <- (u > -1 & u < 1)
+        result[logic0] <- 0
+        Uval <- result[logic1]
+        result[logic1] <- (15 / 16) * ((1 - (Uval^2)))^2
+        result
+      } else {
+        if (type_kernel == "t") {
+          result <- u
+          logic0 <- (u <= -1 | u >= 1)
+          logic1 <- (u > -1 & u < 1)
+          result[logic0] <- 0
+          Uval <- result[logic1]
+          result[logic1] <- (35 / 32) * ((1 - (Uval^2)))^3
+          result
+        }
+      }
+    }
+  }
 }
